d3fa3948d97684c49ea5219c84dc88faecfe1c96
efeakil <efe.akil35@gmail.com>
2023-05-11 18:17:48 +0300

    initial start

---
+++ src/test/java/com/codersnitch/restcontrollerdemo/RestControllerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codersnitch.restcontrollerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestControllerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
